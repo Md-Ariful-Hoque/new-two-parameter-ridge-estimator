c8df22aad5a930d295d5c4bf118b4d2063593cc5
Md Ariful Hoque <166432759+Md-Ariful-Hoque@users.noreply.github.com>
2026-04-23 22:22:01 -0400

    Implement data generation functions for predictors and response
    Added functions for generating correlated predictors and response variables.

--- R/data_generation.R
+++ R/data_generation.R
@@ -1,4 +1,16 @@
-generation of correlated predictors
-response generation
-beta construction
-correlation structure setup
+# Data generation functions
+
+generate_x <- function(n, p, rho) {
+  z <- matrix(rnorm(n * (p + 1)), nrow = n, ncol = p + 1)
+  x <- matrix(0, nrow = n, ncol = p)
+
+  for (j in 1:p) {
+    x[, j] <- sqrt(1 - rho^2) * z[, j] + rho * z[, p + 1]
+  }
+
+  scale(x)
+}
+
+generate_y <- function(x, beta, sigma) {
+  as.vector(x %*% beta + rnorm(nrow(x), mean = 0, sd = sigma))
+}
