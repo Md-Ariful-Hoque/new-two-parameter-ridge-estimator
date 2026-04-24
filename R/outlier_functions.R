029672a91a36ded11cd6c80b61bfc425511976c6
Md Ariful Hoque <166432759+Md-Ariful-Hoque@users.noreply.github.com>
2026-04-24 11:37:55 -0400

    Refactor outlier function to improve clarity

--- R/outlier_functions.R
+++ R/outlier_functions.R
@@ -1,12 +1,4 @@
-# Outlier contamination functions
-
-add_outliers <- function(y, proportion = 0.25) {
-  n <- length(y)
-  n_out <- floor(proportion * n)
-
-  out_index <- sample(1:n, n_out)
-  out_value <- quantile(y, 0.75) + 5 * IQR(y)
-
-  y[out_index] <- out_value
-  y
-}
+# Outliers
+    num_out <- round(0.25 * n) # 25% outliers
+    out_ind <- sample(1:n, num_out, replace = F)
+    e[out_ind] <- quantile(e, 0.75) + 5 * IQR(e)
