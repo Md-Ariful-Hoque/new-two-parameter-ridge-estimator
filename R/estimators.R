680ca3d7c936409a09edd2dde18944c0b1978475
Md Ariful Hoque <166432759+Md-Ariful-Hoque@users.noreply.github.com>
2026-04-23 22:29:28 -0400

    Add multiple two-parameter estimator functions

--- R/estimators.R
+++ R/estimators.R
@@ -1,19 +1,77 @@
-# Estimator functions
+# Two-parameter estimator functions
 
 ols_estimator <- function(x, y) {
   solve(t(x) %*% x) %*% t(x) %*% y
 }
 
-ridge_estimator <- function(x, y, k) {
-  p <- ncol(x)
-  solve(t(x) %*% x + k * diag(p)) %*% t(x) %*% y
+lte_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + k * diag(p)) %*% (xtx - d * diag(p)) %*% ols
 }
 
-ntpr_estimator <- function(x, y, k) {
-  xtx <- t(x) %*% x
-  p <- ncol(x)
+tp_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + k * diag(p)) %*% (xtx + k * d * diag(p)) %*% ols
+}
 
-  ols <- ols_estimator(x, y)
+nbe_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + diag(p)) %*%
+    (xtx + (d + k) * diag(p)) %*%
+    solve(xtx + k * diag(p)) %*%
+    xtx %*% ols
+}
+
+yc_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + diag(p)) %*%
+    (xtx + d * diag(p)) %*%
+    solve(xtx + k * diag(p)) %*%
+    xtx %*% ols
+}
+
+mrt_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + k * (1 + d) * diag(p)) %*% xtx %*% ols
+}
+
+dk_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + k * (1 + d) * diag(p)) %*%
+    (xtx - k * (1 + d) * diag(p)) %*% ols
+}
+
+gtp_estimator <- function(xtx, ols, k, d_vec) {
+  p <- ncol(xtx)
+  D <- diag(d_vec)
+  solve(xtx + k * diag(p)) %*% (xtx + k * D) %*% ols
+}
+
+lkl_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  C <- solve(xtx + diag(p)) %*% (xtx + d * diag(p))
+  A <- solve(xtx + k * diag(p)) %*% (xtx - k * diag(p))
+  C %*% A %*% ols
+}
+
+ntp_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + k * d * diag(p)) %*%
+    (xtx - k * d * diag(p)) %*% ols
+}
+
+nrt_estimator <- function(xtx, ols, k, d) {
+  p <- ncol(xtx)
+  solve(xtx + (k + d) * diag(p)) %*% xtx %*% ols
+}
+
+tpr_estimator <- function(xtx, ols, k, q) {
+  p <- ncol(xtx)
+  q * solve(xtx + k * diag(p)) %*% xtx %*% ols
+}
+
+ntpr_estimator <- function(xtx, ols, k) {
+  p <- ncol(xtx)
 
   q_min <- min(eigen((xtx + k * diag(p)) %*% solve(xtx),
                      symmetric = TRUE,
