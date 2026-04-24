880e141b1f1ed9a12ccd4eeeeef06a88b775979f
Md Ariful Hoque <166432759+Md-Ariful-Hoque@users.noreply.github.com>
2026-04-23 22:22:28 -0400

    Implement OLS, Ridge, and NTPR estimator functions
    Added functions for OLS, Ridge, and NTPR estimators.

--- R/estimators.R
+++ R/estimators.R
@@ -1,16 +1,23 @@
-OLS
-TP1 / TP2
-NBE1 / NBE2
-YC1 / YC2 / YC3
-MAUTP
-MRT
-DK
-GTP
-MLRT
-NBR
-BTP
-NTP
-NRT
-LKL
-TPR
-NTPR
+# Estimator functions
+
+ols_estimator <- function(x, y) {
+  solve(t(x) %*% x) %*% t(x) %*% y
+}
+
+ridge_estimator <- function(x, y, k) {
+  p <- ncol(x)
+  solve(t(x) %*% x + k * diag(p)) %*% t(x) %*% y
+}
+
+ntpr_estimator <- function(x, y, k) {
+  xtx <- t(x) %*% x
+  p <- ncol(x)
+
+  ols <- ols_estimator(x, y)
+
+  q_min <- min(eigen((xtx + k * diag(p)) %*% solve(xtx),
+                     symmetric = TRUE,
+                     only.values = TRUE)$values)
+
+  q_min * solve(xtx + k * diag(p)) %*% xtx %*% ols
+}
