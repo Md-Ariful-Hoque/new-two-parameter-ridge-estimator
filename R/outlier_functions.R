5d5359edace0e27a986fa6b6b95f1a38b4e20f41
Md Ariful Hoque <166432759+Md-Ariful-Hoque@users.noreply.github.com>
2026-04-23 22:23:42 -0400

    Add outlier contamination function
    Added a function to generate outlier contamination in a dataset.

--- R/outlier_functions.R
+++ R/outlier_functions.R
@@ -1,2 +1,12 @@
-25% outlier contamination generation
-robust comparison support
+# Outlier contamination functions
+
+add_outliers <- function(y, proportion = 0.25) {
+  n <- length(y)
+  n_out <- floor(proportion * n)
+
+  out_index <- sample(1:n, n_out)
+  out_value <- quantile(y, 0.75) + 5 * IQR(y)
+
+  y[out_index] <- out_value
+  y
+}
