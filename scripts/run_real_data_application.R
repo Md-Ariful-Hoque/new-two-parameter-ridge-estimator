3dda69bb1e3414698747efd18a5cd37125307723
Md Ariful Hoque <166432759+Md-Ariful-Hoque@users.noreply.github.com>
2026-04-23 22:37:44 -0400

    Update real data application to use new analysis script

--- scripts/run_real_data_application.R
+++ scripts/run_real_data_application.R
@@ -1,16 +1,7 @@
-library(readxl)
+# Run real-data application
 
-source("../R/data_loading.R")
-source("../R/estimators_two_parameter.R")
+source("../R/real_data_analysis.R")
 
-data <- load_bodyfat()
+real_data_results <- run_real_data_estimators()
 
-# Adjust based on your dataset
-y <- data[[1]]
-X <- as.matrix(data[, -1])
-
-ols <- ols_estimator(X, y)
-ntpr <- ntpr_estimator(t(X) %*% X, ols, k = 0.1)
-
-print(ols)
-print(ntpr)
+print(real_data_results)
